e35406e3f306bfbe68d0dd04b86f68528845fd49
rrQuintana <113273490+rrQuintana@users.noreply.github.com>
2023-02-17 16:33:14 -0600

    qwe

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,6 +1,9 @@
 import React from "react";
 import imagenProducto from "../../assets/img/img-prod.png";
 import ProductCart from "./ProductCart";
+import { db } from "../firebase.config";
+import { collection, addDoc, query, getDocs } from "firebase/firestore";
+
 
 function ProductList() {
   const products = [
@@ -11,86 +14,38 @@ function ProductList() {
       Titulo: "Titulo",
       Item1: "Item 1",
     },
-    {
-      id: 2,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo2",
-      Item1: "Item 1",
-      Item2: "Item 2",
-    },
-    {
-      id: 3,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo3",
-      Item1: "Item 1",
-      Item2: "Item 2",
-      Item3: "Item 3",
-    },
-    {
-      id: 4,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo4",
-      Item1: "Item 1",
-      Item2: "Item 2",
-      Item3: "Item 3",
-      Item4: "Item 4",
-    },
-    {
-      id: 5,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo5",
-      Item1: "Item 1",
-      Item2: "Item 2",
-      Item3: "Item 3",
-      Item4: "Item 4",
-      Item5: "6+",
-    },
-    {
-      id: 5,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo6",
-      Item1: "Item 1",
-      Item2: "Item 2",
-      Item3: "Item 3",
-      Item4: "Item 4",
-      Item5: "6+",
-    },
-    {
-      id: 5,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo7",
-      Item1: "Item 1",
-      Item2: "Item 2",
-      Item3: "Item 3",
-      Item4: "Item 4",
-      Item5: "6+",
-    },
-    {
-      id: 5,
-      LinkTo: "https://google.com",
-      Imagen: { imagenProducto },
-      Titulo: "Titulo8",
-      Item1: "Item 1",
-      Item2: "Item 2",
-      Item3: "Item 3",
-      Item4: "Item 4",
-      Item5: "6+",
-    },
   ];
+
+  async function Listar(){
+    const q = query(collection(db, "productos"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      const productData = {
+        id: doc.id,
+        LinkTo: "https://google.com",
+        Imagen: { imagenProducto },
+        Titulo: doc.Nombre,
+        Item1: doc.Peticion,
+      }
+      products.push(productData);
+      console.log("products: ");
+      console.log(products);
+      
+    });
+  }
+  
 
   return (
     <div className="h-custom d-flex row products-list justify-content-center align-items-center">
       <h1>Productos</h1>
+      <button className="btn-success" onClick={Listar}>Listar</button>
       <div className="container-fluid d-flex m-3 flex-wrap justify-content-center align-items-center">
-        {products.map((product, index) => {
-          return <ProductCart key={index} {...product} />;
-        })}
+        {products.map((product, index) => {          
+        return <ProductCart key={index} {...product} />;
+      })}
       </div>
     </div>
   );
