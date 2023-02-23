20f822df564946a88f41c67804688beb766e7e73
rrQuintana <113273490+rrQuintana@users.noreply.github.com>
2023-02-23 12:32:47 -0600

    Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import backgroundImage from "./background.jpg";
 import { useNavigate } from "react-router-dom";
-import { NavBar } from "../LandingPage/NavBar";
 import googleicon from "./google-icon.svg";
 import { auth, provider } from "../firebase.config";
 import {
@@ -13,7 +14,7 @@ import {
 import { AuthContext } from "../../AuthContext";
 
 function Login() {
-  const [registro, setRegistro] = useState(false); //Manejadores de estado para registrar o logear
+  const [registro, setRegistro] = useState(true); //Manejadores de estado para registrar o logear
 
   //Data de login con Google
   const auth = getAuth();
@@ -53,7 +54,10 @@ function Login() {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        console.log("Usuario creado: ", user);
+        window.alert(user.email + " registrado con exito");
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        setRegistro(!registro);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -83,60 +87,113 @@ function Login() {
   }
 
   return (
-    <div>
-      <NavBar></NavBar>
-      <h1>Esta es la página de login</h1> <br />
-      {!registro ? (
-        <>
-          <h2>Log In</h2>
-          <input type="email" placeholder="correo" id="email" required /> <br />
-          <input
-            type="passwprd"
-            placeholder="contraseña"
-            id="password"
-            required
-          />{" "}
-          <br /> <br />
-          <button className="btn btn-primary" onClick={Ingresar}>Iniciar sesión</button>
-          <p>------------ o entra usando ------------</p>
-          <a href="#" onClick={logIn}>
-            {" "}
-            <img src={googleicon} alt="google-icon" />{" "}
-          </a>
-          <p>
-            No tienes cuenta?{" "}
-            <a href="#" onClick={() => setRegistro(!registro)}>
-              Registrate
-            </a>
-          </p>
-        </>
-      ) : (
-        <>
-          <h2>Sign In</h2>
-          <input type="email" placeholder="correo" required id="email" /> <br />
-          <input
-            type="password"
-            placeholder="contraseña"
-            id="password"
-            required
-          />{" "}
-          <br /> <br />
-          <button className="btn btn-primary" onClick={Registrar}>
-            Registrarse
-          </button>
-          <p>------------ o entra usando ------------</p>
-          <a href="#" onClick={logIn}>
-            {" "}
-            <img src={googleicon} alt="google-icon" />{" "}
-          </a>
-          <p>
-            Ya tienes cuenta?{" "}
-            <a href="#" onClick={() => setRegistro(!registro)}>
-              Ingresa
-            </a>
-          </p>
-        </>
-      )}
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        height: "100vh",
+        padding: "2rem",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <div
+              style={{ backgroundColor: "white", padding: "2rem" }}
+              className="mt-3"
+            >
+              <div className="d-flex">
+                <h1 className="text-black ms-3">Xchange Life</h1>
+                <a href="#" className="m-auto me-5 text-secondary" onClick={()=>{navigate("/");}}>Regresar</a>
+              </div>
+
+              <br />
+              {registro ? (
+                <h2 className="text-black my-3">Login</h2>
+              ) : (
+                <h2 className="text-black my-3">Sign in</h2>
+              )}
+              <Form>
+                <Form.Group>
+                  <h5 className="text-secondary">Correo</h5>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    id="email"
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <h5 className="text-secondary mt-4">Contraseña</h5>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    required
+                  />
+                </Form.Group>
+
+                <div className="d-flex d-flex row justify-content-center align-items-center">
+                  {registro ? (
+                    <>
+                      <Button variant="primary" className="mt-3" onClick={Ingresar}>
+                        Ingresar
+                      </Button>
+                      <p className="text-secondary mt-3 d-flex justify-content-center align-items-center">
+                        No tienes cuenta?{" "}
+                        <a
+                          href="#"
+                          onClick={() => {
+                            setRegistro(!registro);
+                          }}
+                        >
+                          {" "}
+                          Registrate
+                        </a>
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <Button variant="primary" className="mt-3" onClick={Registrar}>
+                        Registrar
+                      </Button>
+                      <p className="text-secondary mt-3 d-flex justify-content-center align-items-center">
+                        Ya tienes cuenta?{" "}
+                        <a
+                          href="#"
+                          onClick={() => {
+                            setRegistro(!registro);
+                          }}
+                        >
+                          {" "}
+                          Ingresa
+                        </a>
+                      </p>
+                    </>
+                  )}
+
+                  <hr className="text-black" />
+                  <p className="text-secondary d-flex justify-content-center align-items-center">
+                    O inicia sesión usando
+                  </p>
+                  <a
+                    href="#"
+                    className="d-flex justify-content-center align-items-center"
+                  >
+                    <img
+                      src={googleicon}
+                      style={{ width: 60 }}
+                      alt="google-icon"
+                      onClick={logIn}
+                    />
+                  </a>
+                </div>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
